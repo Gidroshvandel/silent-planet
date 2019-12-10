451d62bc5b4f84267762e31b9aabcc3313c352b1
Gidroshvandel <gidroshvandel@gmail.com>
2019-12-10 22:33:47 +0300

    removed unused method

--- app/src/main/java/com/silentgames/silent_planet/engine/LayerEntityMover.kt
+++ app/src/main/java/com/silentgames/silent_planet/engine/LayerEntityMover.kt
@@ -9,7 +9,7 @@ class LayerEntityMover(private val position: Int, private val newLayer: Layer) {
         val oldLayer = scene.getLayer(position)
         if (oldLayer != null) {
             if (newLayer.data.firstOrNull { it is Entity } != null) {
-                scene.setLayer(position, getChangedList(oldLayer, newLayer))
+                scene.setLayer(position, moveChangedEntities(oldLayer, newLayer))
             } else {
                 scene.setLayer(position, newLayer)
             }
@@ -19,20 +19,6 @@ class LayerEntityMover(private val position: Int, private val newLayer: Layer) {
     }
 
     private fun moveChangedEntities(oldLayer: Layer, newLayer: Layer): Layer {
-        val oldLayerData = oldLayer.data.filterIsInstance<Entity>()
-        val newLayerData = newLayer.data.filterIsInstance<Entity>()
-
-        oldLayerData.forEach { oldEntity ->
-            newLayerData.forEach { newEntity ->
-                if (oldEntity == newEntity && oldEntity.axis != newEntity.axis) {
-                    newEntity.move(oldEntity.axis, newEntity.axis)
-                }
-            }
-        }
-        return newLayer
-    }
-
-    private fun getChangedList(oldLayer: Layer, newLayer: Layer): Layer {
         val oldLayerData = oldLayer.data.filterIsInstance<Entity>()
         val newLayerData = newLayer.data.filterIsInstance<Entity>()
 
