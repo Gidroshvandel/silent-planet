7d399194a3a3e5491dc0d4954e93791427980040
Gidroshvandel <gidroshvandel@gmail.com>
2019-12-11 23:20:37 +0300

    refactored constants

---
+++ app/src/main/java/com/silentgames/silent_planet/logic/Constants.kt
@@ -0,0 +1,12 @@
+package com.silentgames.silent_planet.logic
+
+/**
+ * Created by gidroshvandel on 09.07.16.
+ */
+object Constants {
+    const val horizontalCountOfCells = 12
+    const val verticalCountOfCells = 12
+    const val cellImageSize = 31f
+    const val entityImageSize = 20f
+    const val countCrystalsToWin = 20
+}
