47300187dc3d2f2dd41d60a513906752585b1686
Gidroshvandel <gidroshvandel@gmail.com>
2020-01-21 11:10:35 +0300

    added arrow rotation

--- graphic/src/com/silentgames/graphic/engine/ArrowBackground.kt
+++ graphic/src/com/silentgames/graphic/engine/ArrowBackground.kt
@@ -1,5 +1,6 @@
 package com.silentgames.graphic.engine
 
+import com.badlogic.gdx.graphics.g2d.Sprite
 import com.silentgames.core.logic.ecs.component.RotateAngle
 
 class ArrowBackground(
@@ -8,20 +9,20 @@ class ArrowBackground(
         private val rotateAngle: RotateAngle
 ) : Background(axis, bmpId) {
 
-//    override fun initBitmap(bmpResourceId: String): Texture {
-//        return super.initBitmap(bmpResourceId).rotateBitmap(rotateAngle)
-//    }
+    override fun getResizedBitmap(width: Float, height: Float): Sprite {
+        return super.getResizedBitmap(width, height).rotateBitmap(rotateAngle)
+    }
 
-//    private fun Texture.rotateBitmap(rotateAngle: RotateAngle): Texture {
-//        val matrix = Matrix()
-//        when (rotateAngle) {
-//            RotateAngle.DEGREES90 -> matrix.postRotate(90f)
-//            RotateAngle.DEGREES180 -> matrix.postRotate(180f)
-//            RotateAngle.DEGREES270 -> matrix.postRotate(270f)
-//            RotateAngle.DEGREES0 -> matrix.postRotate(0f)
-//        }
-//        return Texture.createBitmap(this, 0, 0, this.width, this.height, matrix, true)
-//    }
+    private fun Sprite.rotateBitmap(rotateAngle: RotateAngle): Sprite {
+        this.setOriginCenter()
+        when (rotateAngle) {
+            RotateAngle.DEGREES90 -> this.setRotation(90f)
+            RotateAngle.DEGREES180 -> this.setRotation(180f)
+            RotateAngle.DEGREES270 -> this.setRotation(270f)
+            RotateAngle.DEGREES0 -> this.setRotation(0f)
+        }
+        return this
+    }
 
     override fun getBitmapId(): Int = (super.getBitmapId().toString() + rotateAngle.name).hashCode()
 
